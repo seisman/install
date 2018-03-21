489f97e3247115eb814a3270d9390f01060093da
Dongdong Tian <seisman.info@gmail.com>
2018-03-21 10:07:01 +0800

    update pandoc script

--- install-pandoc.sh
+++ install-pandoc.sh
@@ -4,4 +4,4 @@ REPO=https://github.com/jgm/pandoc
 VERSION=2.1.3
 
 curl -sSL ${REPO}/releases/download/${VERSION}/${NAME}-${VERSION}-linux.tar.gz \
-    | tar -xz --strip-components 2 ${NAME}-${VERSION}/bin/pandoc
+    | tar -xz --strip-components 2 ${NAME}-${VERSION}/bin/pandoc ${NAME}-${VERSION}/bin/pandoc-citepro
