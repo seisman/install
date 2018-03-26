9dd48fb6b51ed9511add8d92d6645f7b7cb637d2
Dongdong Tian <seisman.info@gmail.com>
2018-03-26 17:06:14 +0800

    fix typo

--- install-SourceCodePro.sh
+++ install-SourceCodePro.sh
@@ -3,5 +3,5 @@ DEST=/usr/share/fonts/opentype
 REPO=https://github.com/adobe-fonts/source-code-pro.git
 BRANCH=release
 
-git clone --depth 1 --branch BRANCH ${REPO} ${DEST}
+git clone --depth 1 --branch ${BRANCH} ${REPO} ${DEST}
 fc-cache -f -v
