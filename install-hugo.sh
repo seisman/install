e1969fb888ee09d0536820c341cbc873f66987de
Dongdong Tian <seisman.info@gmail.com>
2018-07-05 15:24:34 +0800

    bump hugo to 0.42.2

--- install-hugo.sh
+++ install-hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NAME=hugo
 REPO=https://github.com/gohugoio/hugo
-VERSION=0.42.1
+VERSION=0.42.2
 
 curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
