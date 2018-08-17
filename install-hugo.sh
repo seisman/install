8388cf44575c396ee99bf4d748b64150a3ae56e9
Dongdong Tian <seisman.info@gmail.com>
2018-08-17 13:55:29 -0400

    Update hugo to 0.47

--- install-hugo.sh
+++ install-hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NAME=hugo
 REPO=https://github.com/gohugoio/hugo
-VERSION=0.46
+VERSION=0.47
 
 curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
