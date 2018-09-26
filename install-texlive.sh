a61742fc114579523b61c4f2e1363fc0a4a720f2
Dongdong Tian <seisman.info@gmail.com>
2018-09-26 10:01:43 -0400

    Add one more texlive package

--- install-texlive.sh
+++ install-texlive.sh
@@ -30,3 +30,4 @@ echo 'export PATH=$PATH':$TEXBIN >> ~/.bash_profile
 $TEXBIN/tlmgr install tabulary latexmk ulem environ trimspaces titlesec \
                       varwidth framed threeparttable wrapfig upquote capt-of \
                       multirow eqparbox needspace fncychap enumitem tracklang \
+                      xindy
