702717ff0843fd0ee1067201e39904d3aa7243cd
Dongdong Tian <seisman.info@gmail.com>
2019-02-17 13:15:30 -0500

    Update htmltest 0.10.1

--- install-htmltest.sh
+++ install-htmltest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NAME=htmltest
 REPO=https://github.com/wjdp/htmltest
-VERSION=0.9.1
+VERSION=0.10.1
 
 curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_${VERSION}_linux_amd64.tar.gz | tar -xzv ${NAME}
