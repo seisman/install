c7fba7dde55d0c834ba7423b5371ca2b9c6557ce
Dongdong Tian <seisman.info@gmail.com>
2019-05-11 21:40:52 -0400

    Update pandoc

--- install-pandoc.sh
+++ install-pandoc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAME=pandoc
 REPO=https://github.com/jgm/pandoc
-VERSION=2.6
+VERSION=2.7.2
 
 curl -sSL ${REPO}/releases/download/${VERSION}/${NAME}-${VERSION}-linux.tar.gz \
     | tar -xz --strip-components 2 ${NAME}-${VERSION}/bin/pandoc ${NAME}-${VERSION}/bin/pandoc-citeproc
