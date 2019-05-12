b0bd39213d3522ab074818aa728ed10d7a2d6f0f
Dongdong Tian <seisman.info@gmail.com>
2019-05-11 21:39:23 -0400

    Fix hugo

--- install-hugo.sh
+++ install-hugo.sh
@@ -3,4 +3,4 @@ NAME=hugo
 REPO=https://github.com/gohugoio/hugo
 VERSION=0.55.5
 
-curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_extended_${VERSION}_Linux-64bit.tar.gz | tar -xv ${NAME}
+curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_extended_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
