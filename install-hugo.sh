5f3933fbbd47b873239f0601ec39f1cf6fc3c9b2
Dongdong Tian <seisman.info@gmail.com>
2019-05-21 11:43:02 -0400

    hugo extended

--- install-hugo.sh
+++ install-hugo.sh
@@ -3,4 +3,5 @@ NAME=hugo
 REPO=https://github.com/gohugoio/hugo
 VERSION=0.55.6
 
-curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
+curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_extended_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
+#curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
