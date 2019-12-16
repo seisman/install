c94a9f1b6ca9a0231cff669d023006a87f744216
Dongdong Tian <seisman.info@gmail.com>
2019-12-16 15:15:19 -0500

    hugo 0.61.0

--- install-hugo.sh
+++ install-hugo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAME=hugo
 REPO=https://github.com/gohugoio/hugo
-VERSION=0.55.6
+VERSION=0.61.0
 
 curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_extended_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
 #curl -sSL ${REPO}/releases/download/v${VERSION}/${NAME}_${VERSION}_Linux-64bit.tar.gz | tar -xzv ${NAME}
